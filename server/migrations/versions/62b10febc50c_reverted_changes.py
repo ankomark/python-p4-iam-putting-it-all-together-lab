cd4aad4fa671a5b510c344ae9b6200f825c1272b
ankomark <marksilas092@gmail.com>
2024-10-22 13:17:21 -0700

    .

---
+++ server/migrations/versions/62b10febc50c_reverted_changes.py
@@ -0,0 +1,24 @@
+"""reverted changes
+
+Revision ID: 62b10febc50c
+Revises: 98f2ab5c4576
+Create Date: 2024-10-14 16:08:20.743738
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '62b10febc50c'
+down_revision = '98f2ab5c4576'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ server/migrations/versions/98f2ab5c4576_testing_hash.py
@@ -0,0 +1,24 @@
+"""testing hash
+
+Revision ID: 98f2ab5c4576
+Revises: d97da9b70273
+Create Date: 2024-10-14 16:07:22.067063
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '98f2ab5c4576'
+down_revision = 'd97da9b70273'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
